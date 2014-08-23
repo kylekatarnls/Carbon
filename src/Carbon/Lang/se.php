e1dfcec8325ef0ab314960aaba629f8511464f33
Jimmie Johansson <info@lagenheter24.se>
2014-08-23 13:42:48 +0200

    Create swedish

---
+++ src/Carbon/Lang/se.php
@@ -0,0 +1,35 @@
+<?php
+
+return array(
+	'dayDateTimeFormat'	=> 'H:i, D j M, Y',
+	'formattedDate'		=> 'j M, Y',
+
+	'sunday'	=> 'söndag',
+	'monday'	=> 'måndag',
+	'tuesday'	=> 'tisdag',
+	'wednesday'	=> 'onsdag',
+	'thursday'	=> 'torsdag',
+	'friday'	=> 'fredag',
+	'saturday'	=> 'lördag',
+
+	'second'	=> 'sekund',
+	'minute'	=> 'minut',
+	'hour'		=> 'timma',
+	'day'		=> 'dag',
+	'week'		=> 'vecka',
+	'month'		=> 'månad',
+	'year'		=> 'år',
+
+	'second-pl'	=> 'sekunder',
+	'minute-pl'	=> 'minuter',
+	'hour-pl'	=> 'timmar',
+	'day-pl'	=> 'dagar',
+	'week-pl'	=> 'veckor',
+	'month-pl'	=> 'månader',
+	'year-pl'	=> 'år',
+
+	'fromNow'	=> 'om {diff}',
+	'ago'		=> '{diff} sedan',
+	'after'		=> '{diff} efter',
+	'before'	=> '{diff} före'
+);
